107c3dce7dd325274eeb826ad52153012f36db0f
turtles4all <jadki10@wgu.edu>
2017-04-14 06:31:37 -0700

    Update XOR.py

--- XOR.py
+++ XOR.py
@@ -1,32 +1,21 @@
-import sys
+#!/usr/bin/env python
 import os
 import argparse
 import hashlib
 import base64
 
 def main(args):
-    logo = base64.b64decode(
-        "Cl9fICBfX19fXyAgX19fXyAgClwgXC8gLyBfIFx8ICBfIFwgCiBcICAvIHwgfCB8IHxfKSB8CiAvICBcIHxffCB8ICBfIDwgCi9fL1xfXF9fXy98X3wgXF9cCiAgICAgICAgICAgICAgICAgCg==")
-    print(logo.decode('ascii'))
+    print(logo())
     fileIn = args.fileIn
     fileOut = args.fileOut
+    confirm_args(fileIn, fileOut, args.keyString)
     key = hash_key(args.keyString)
 
-    print("Input File -- : \"{}\"\n"
-          "Output File - : \"{}\"\n"
-          "Key --------- : \"{}\"".format(fileIn, fileOut, args.keyString))
-    while True:
-        confirm = input("Is this correct? (YES/NO):")
-        if confirm.upper() == "NO":
-            exit()
-        elif confirm.upper() == "YES":
-            break
-        else:
-            print("input must be \"YES\" or \"NO\"")
     try:
         f = open(fileIn, 'rb')
         try:
             out = open(fileOut, 'x')
+            out = open(fileOut, 'w+b')
         except IOError:
             overw = input("Overwrite file \"{}\"? (Y/N)".format(fileOut))
             if overw.upper() == 'Y':
@@ -44,7 +33,7 @@ def main(args):
         bytesRead = 0
         wright = True
         while wright:
-            print("{comp}% complete".format(comp=("%0.2f" % (bytesRead / fileSize * 100))), end='\r')
+            print("{comp}% complete".format(comp=("%0.1f" % (bytesRead / fileSize * 100))), end='\r')
             block = f.read(blockSize)
             if block:
                 blockLen = len(block)
@@ -55,7 +44,7 @@ def main(args):
             else:
                 wright = False
 
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, IOError):
         f.close()
         out.close()
         os.remove(fileOut)
@@ -64,6 +53,23 @@ def main(args):
     print("\nXOR finished writing to file \"{}\"".format(fileOut))
     exit()
 
+def confirm_args(fileIn, fileOut, string):
+    print("Input File -- : \"{}\"\n"
+          "Output File - : \"{}\"\n"
+          "Key --------- : \"{}\"".format(fileIn, fileOut, string))
+    while True:
+        confirm = input("Is this correct? (YES/NO):")
+        if confirm.upper() == "NO":
+            exit()
+        elif confirm.upper() == "YES":
+            break
+        else:
+            print("input must be \"YES\" or \"NO\"")
+
+def logo():
+    logo = base64.b64decode(
+        "Cl9fICBfX19fXyAgX19fXyAgClwgXC8gLyBfIFx8ICBfIFwgCiBcICAvIHwgfCB8IHxfKSB8CiAvICBcIHxffCB8ICBfIDwgCi9fL1xfXF9fXy98X3wgXF9cCiAgICAgICAgICAgICAgICAgCg==")
+    return logo.decode('ascii')
 
 def parse_args():
     argParse = argparse.ArgumentParser(description='XOR an input with a given key')
